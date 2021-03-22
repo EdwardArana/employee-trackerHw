af7177cc9419b303bc51091c61d07b9fe1420b93
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-22 14:49:33 -0700

    doing the server js stuff

---
+++ tracker.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password:"",
+
+    database: "employees_db"
+    
+})
